40f128eb0b31fdfac049c28e7262fb82e8958cda
charlenelam1209 <charlenelam@ocadu.ca>
2025-01-24 02:04:16 -0500

    first version

---
+++ sctm 2005/week 2/script.js
@@ -0,0 +1,42 @@
+0; // this is a comment, which means that javascript will not try to execute part
+134151855;
+12341.1515; //no difference between integers and floats
+Infinity;
+
+var kitty; // declared  a variable for store space in memory;
+
+kitty = 5; // deefinitionwith a data type of number with the value 5
+
+var Kitty = 24; // semicolons are end of statement, this line has a definition and declaration
+
+console.log(kitty);
+
+kitty = Kitty; // = assignment operator right to left
+
+console.log(kitty);
+
+var doggy; // declared a variable but not defined a value
+console.log(doggy); // undefined
+
+// numbers, undefined, strings
+
+var prose = 'hjgfgdsdjklhgfhdnm,bhgfdsmlkjnhbgvfcxd'; // ' is a string delimiter
+var moreprose = "i said 'somethin' jlkhgdfw ";
+
+// booleans are either true or false
+
+var x = true;
+
+x = false;
+
+//objects
+var ball = {}; // empty object
+ball = null; // null is a datatype in jaascript
+
+// operators
+5 + 5;
+kitty = 5 + 5;
+var kitty ; // becomes undefined because we are now referring to a new place
+
+kitty = 5 + "5";
+console.log(kitty);
